8614377e80ff157cf631682d3072b98d5ef39e02
yeo50 <minhtet3294@gmail.com>
2024-04-24 21:34:58 +0630

    change cartItems and delete items added

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,9 +1,16 @@
+/* eslint-disable linebreak-style */
 export const parseRequestUrl = () => {
-  const url = document.location.hash.toLowerCase();
-  const request = url.split("/");
-  return {
-    resource: request[1],
-    id: request[2],
-    action: request[3],
-  };
-};
+    const url = document.location.hash.toLowerCase()
+    const request = url.split('/')
+    return {
+        resource: request[1],
+        id: request[2],
+        action: request[3],
+    }
+}
+
+export const rerender = async (component) => {
+    document.getElementById('main-container').innerHTML =
+        await component.render()
+    await component.after_render()
+}
